ec706e8bc99cf0241198e6408ba7f78a66841316
Suyog47 <suyogamin11@gmail.com>
2023-10-11 12:15:50 +0530

    include pull to refresh feature

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:water_timing/constants/colors.dart';
 import 'package:water_timing/constants/heights.dart';
 import 'package:water_timing/constants/size_helpers.dart';
@@ -25,6 +26,16 @@ class _DashboardState extends State<Dashboard> {
   final DataController _dataController = Get.find();
   late List<TableRow> header = [];
   late List<TableRow> arr = [];
+  final RefreshController _refreshController = RefreshController(initialRefresh: false);
+
+  @override
+  void initState() {
+    setTableHeader();
+    setTimings(_dataController.timings);
+    areaNotifier = ValueNotifier(_dataController.selectedArea ?? _dataController.areas[0]);
+    _newsTextController.text = _dataController.news.value;
+    super.initState();
+  }
 
   void setTableHeader() {
     header = [
@@ -71,15 +82,6 @@ class _DashboardState extends State<Dashboard> {
                 ))
           ]),
     ];
-  }
-
-  @override
-  void initState() {
-    setTableHeader();
-    setTimings(_dataController.timings);
-    areaNotifier = ValueNotifier(_dataController.selectedArea ?? _dataController.areas[0]);
-    _newsTextController.text = _dataController.news.value;
-    super.initState();
   }
 
   void setTimings(List timing) {
@@ -217,6 +219,24 @@ class _DashboardState extends State<Dashboard> {
     }
   }
 
+  Future onRefresh() async {
+    _dataController.load.value = 1;
+    await _dataController.getAllData();
+    if(_dataController.response == 'success'){
+      arr.clear();
+      setTimings(_dataController.timings);
+    }
+    else{
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Oops...something went wrong, please try again later'),
+        ),
+      );
+    }
+    _dataController.load.value = 0;
+    _refreshController.refreshCompleted();
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -262,151 +282,157 @@ class _DashboardState extends State<Dashboard> {
           body: Stack(
             alignment: Alignment.center,
             children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 18.0, right: 18.0, top: 25.0, bottom: 5.0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Column(
-                        children: [
-                          const Text(
-                            "Select an area",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20),
-                          ),
-                          height10,
-                          Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                border: Border.all(color: Colors.grey, width: 2)),
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 5, vertical: 2),
-                            child: ValueListenableBuilder<String>(
-                                valueListenable: areaNotifier,
-                                builder: (_, area, __) {
-                                  return DropdownButton<String>(
-                                    value: area,
-                                    borderRadius: BorderRadius.circular(10),
-                                    onChanged: (newValue) async {
-                                      SharedPreferenceData().setData(newValue!);
-                                      _dataController.selectedArea = newValue;
-                                      areaNotifier.value = newValue;
-                                      _dataController.getTimings(newValue);
-                                      setState(() {
-                                        arr.clear();
-                                        if (_dataController.timings[0]['from'] !=
-                                            null) {
-                                          setTimings(_dataController.timings);
-                                        }
-                                      });
-                                      _newsTextController.text = _dataController.news.value;
-                                    },
-                                    items: _dataController.areas.map<DropdownMenuItem<String>>((value) {
-                                      return DropdownMenuItem<String>(
-                                        value: value,
-                                        child: Text(
-                                          value,
-                                          style: const TextStyle(
-                                              fontWeight: FontWeight.w600),
-                                        ),
-                                      );
-                                    }).toList(),
-                                  );
-                                }),
-                          ),
-                        ],
-                      ),
-                      height30,
-                      (arr.isNotEmpty) ? Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            "Estimated Timings set for this area",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18),
-                          ),
-                          height10,
-                          height5,
-                          Table(
-                            border: TableBorder.all(color: blackColor),
-                            defaultVerticalAlignment:
-                                TableCellVerticalAlignment.middle,
-                            children: arr,
-                          ),
-                        ],
-                      ) : const Text(
-                        "No timings are set for this area",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                      height30,
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            "News:",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20),
-                          ),
-                          height10,
-                          height5,
-                          Container(
-                            height: 120,
-                            decoration: BoxDecoration(
-                                border: Border.all(color: greyColor, width: 2.0),
-                                borderRadius: BorderRadius.circular(5)),
-                            child: Padding(
+              SmartRefresher(
+                enablePullDown: true,
+                header: const WaterDropMaterialHeader(),
+                controller: _refreshController,
+                onRefresh: onRefresh,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 18.0, right: 18.0, top: 25.0, bottom: 5.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Column(
+                          children: [
+                            const Text(
+                              "Select an area",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+                            height10,
+                            Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(color: Colors.grey, width: 2)),
                               padding: const EdgeInsets.symmetric(
-                                  horizontal: 8.0, vertical: 4.0),
-                              child: TextField(
-                                minLines: 8,
-                                maxLines: 10,
-                                controller: _newsTextController,
-                                style: const TextStyle(
-                                  fontSize: 17,
-                                  color: blackColor,
-                                  fontWeight: FontWeight.w500,
+                                  horizontal: 5, vertical: 2),
+                              child: ValueListenableBuilder<String>(
+                                  valueListenable: areaNotifier,
+                                  builder: (_, area, __) {
+                                    return DropdownButton<String>(
+                                      value: area,
+                                      borderRadius: BorderRadius.circular(10),
+                                      onChanged: (newValue) async {
+                                        SharedPreferenceData().setData(newValue!);
+                                        _dataController.selectedArea = newValue;
+                                        areaNotifier.value = newValue;
+                                        _dataController.getTimings(newValue);
+                                        setState(() {
+                                          arr.clear();
+                                          if (_dataController.timings[0]['from'] !=
+                                              null) {
+                                            setTimings(_dataController.timings);
+                                          }
+                                        });
+                                        _newsTextController.text = _dataController.news.value;
+                                      },
+                                      items: _dataController.areas.map<DropdownMenuItem<String>>((value) {
+                                        return DropdownMenuItem<String>(
+                                          value: value,
+                                          child: Text(
+                                            value,
+                                            style: const TextStyle(
+                                                fontWeight: FontWeight.w600),
+                                          ),
+                                        );
+                                      }).toList(),
+                                    );
+                                  }),
+                            ),
+                          ],
+                        ),
+                        height30,
+                        (arr.isNotEmpty) ? Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "Estimated Timings set for this area",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18),
+                            ),
+                            height10,
+                            height5,
+                            Table(
+                              border: TableBorder.all(color: blackColor),
+                              defaultVerticalAlignment:
+                                  TableCellVerticalAlignment.middle,
+                              children: arr,
+                            ),
+                          ],
+                        ) : const Text(
+                          "No timings are set for this area",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        height30,
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "News:",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+                            height10,
+                            height5,
+                            Container(
+                              height: 120,
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: greyColor, width: 2.0),
+                                  borderRadius: BorderRadius.circular(5)),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 8.0, vertical: 4.0),
+                                child: TextField(
+                                  minLines: 8,
+                                  maxLines: 10,
+                                  controller: _newsTextController,
+                                  style: const TextStyle(
+                                    fontSize: 17,
+                                    color: blackColor,
+                                    fontWeight: FontWeight.w500,
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                      height30,
-                      InkWell(
-                        onTap: () async {
-                          _dataController.load.value = 1;
-                          await _dataController.saveNews(_newsTextController.text);
-                          await _dataController.getAllData();
-                          _dataController.load.value = 0;
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text('News has been saved'),
-                            ),
-                          );
-                        },
-                        child: Container(
-                          height: 50,
-                          width: displayWidth(context),
-                          decoration: BoxDecoration(
-                              color: themeBlueColor,
-                              borderRadius: BorderRadius.circular(8)),
-                          child: const Center(
-                            child: Text(
-                              "Add News",
-                              style: TextStyle(
-                                  color: whiteColor,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20),
-                            ),
-                          ),
+                          ],
                         ),
-                      )
-                    ],
+                        height30,
+                        InkWell(
+                          onTap: () async {
+                            _dataController.load.value = 1;
+                            await _dataController.saveNews(_newsTextController.text);
+                            await _dataController.getAllData();
+                            _dataController.load.value = 0;
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('News has been saved'),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            height: 50,
+                            width: displayWidth(context),
+                            decoration: BoxDecoration(
+                                color: themeBlueColor,
+                                borderRadius: BorderRadius.circular(8)),
+                            child: const Center(
+                              child: Text(
+                                "Add News",
+                                style: TextStyle(
+                                    color: whiteColor,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
